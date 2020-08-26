560048f6d2c6a9bec7f497fa7bf37679c9b8d955
Abel237 <abodobrice@gmail.com>
2020-08-26 17:28:55 +0500

    Updates

---
+++ precache-manifest.29fb85a6a3174126c066613dedce5354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435cc8ca9e26c7bd0882d34118f7d3bd",
+    "url": "/boodscode237/cats-select.git/index.html"
+  },
+  {
+    "revision": "c6fd0673b15eebeb4952",
+    "url": "/boodscode237/cats-select.git/static/css/main.54696ccd.chunk.css"
+  },
+  {
+    "revision": "341877a1db1c0b63ca5e",
+    "url": "/boodscode237/cats-select.git/static/js/2.c4f7bd68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boodscode237/cats-select.git/static/js/2.c4f7bd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fd0673b15eebeb4952",
+    "url": "/boodscode237/cats-select.git/static/js/main.eb91a703.chunk.js"
+  },
+  {
+    "revision": "332c1ce1cb495c13cb5b",
+    "url": "/boodscode237/cats-select.git/static/js/runtime-main.0afaafae.js"
+  }
+]);
